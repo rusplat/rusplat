118d152288ce760a10cbd556094a423da472e94e
rusplat <nsphspkp5j@privaterelay.appleid.com>
2025-11-17 20:35:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const remainingSeconds = seconds % 60;
         const formattedMinutes = String(minutes).padStart(2, '0');
         const formattedSeconds = String(remainingSeconds).padStart(2, '0');
-        return `${formattedMinutes}:${formattedSeconds}`;
+        return ``${formattedMinutes}:${formattedSeconds}``;
     }
 
     function startTimer() {
